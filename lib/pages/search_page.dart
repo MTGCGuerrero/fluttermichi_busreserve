97152ff4c5a839fccfa110764406747b3ac499e9
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-07-25 11:00:46 -0400

    implement dummy db, add provider dependency

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,9 +1,10 @@
 import 'package:bus_reservation_udemy/utils/constants.dart';
 import 'package:bus_reservation_udemy/utils/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({Key? key}) : super(key: key);
 
   @override
   State<SearchPage> createState() => _SearchPageState();
@@ -12,19 +13,31 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   String? fromCity, toCity;
   DateTime? departureDate;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    fromCity = 'Dhaka';
+    toCity = 'Sylhet';
+    departureDate = DateTime.now();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MainDrawer(),
       appBar: AppBar(
-        title: Text("Search"),
+        title: const Text('Search'),
       ),
       body: Form(
-          child: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(8),
-          children: [
-            DropdownButtonFormField<String>(
+        key: _formKey,
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8),
+            children: [
+              DropdownButtonFormField<String>(
                 value: fromCity,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -33,20 +46,24 @@ class _SearchPageState extends State<SearchPage> {
                   return null;
                 },
                 decoration: InputDecoration(
-                    errorStyle: const TextStyle(color: Colors.white)),
-                hint: const Text("From"),
+                  errorStyle: const TextStyle(color: Colors.white),
+                ),
+                hint: const Text('From'),
                 isExpanded: true,
                 items: cities
                     .map((city) => DropdownMenuItem<String>(
-                        value: city, child: Text(city)))
+                          value: city,
+                          child: Text(city),
+                        ))
                     .toList(),
                 onChanged: (value) {
                   fromCity = value;
-                }),
-            const SizedBox(
-              height: 10,
-            ),
-            DropdownButtonFormField<String>(
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              DropdownButtonFormField<String>(
                 value: toCity,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -55,43 +72,77 @@ class _SearchPageState extends State<SearchPage> {
                   return null;
                 },
                 decoration: InputDecoration(
-                    errorStyle: const TextStyle(color: Colors.white)),
-                hint: const Text("To"),
+                  errorStyle: const TextStyle(color: Colors.white),
+                ),
+                hint: const Text('To'),
                 isExpanded: true,
                 items: cities
                     .map((city) => DropdownMenuItem<String>(
-                        value: city, child: Text(city)))
+                          value: city,
+                          child: Text(city),
+                        ))
                     .toList(),
                 onChanged: (value) {
                   toCity = value;
-                }),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                    onPressed: _selectedDate,
-                    child: const Text("Selected Departure Date")),
-                Text(departureDate == null
-                    ? "No date chosen"
-                    : getFormattedDate(departureDate!,
-                        pattern: 'EEE MMM dd, yyyy')),
-              ],
-            ),
-          ],
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: _selectDate,
+                      child: const Text('Select Departure Date'),
+                    ),
+                    Text(departureDate == null
+                        ? 'No date chosen'
+                        : getFormattedDate(departureDate!,
+                            pattern: 'EEE MMM dd, yyyy')),
+                  ],
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    onPressed: _search,
+                    child: const Text('SEARCH'),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 
-  void _selectedDate() async {
+  void _selectDate() async {
     final selectedDate = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime.now(),
-        lastDate: DateTime.now().add(const Duration(days: 7)));
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(const Duration(days: 7)),
+    );
     if (selectedDate != null) {
       setState(() {
         departureDate = selectedDate;
+      });
+    }
+  }
+
+  void _search() {
+    if (departureDate == null) {
+      showMsg(context, emptyDateErrMessage);
+      return;
+    }
+    if (_formKey.currentState!.validate()) {
+      Provider.of<AppDataProvider>(context, listen: false)
+          .getRouteByCityFromAndCityTo(fromCity!, toCity!)
+          .then((route) {
+        Navigator.pushNamed(context, routeNameSearchResultPage,
+            arguments: [route, getFormattedDate(departureDate!)]);
       });
     }
   }
